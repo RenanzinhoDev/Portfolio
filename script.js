a6a2097639f9855ed1eb41abad4e76f5405868e6
RenanzinhoDev <126684225+RenanzinhoDev@users.noreply.github.com>
2023-06-28 12:09:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new typed("span",{
+    strings:["Elizabeth Elmiz.","web Developer.","UX Designer.","Photographer."],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true
+});
+Footer
+
+
